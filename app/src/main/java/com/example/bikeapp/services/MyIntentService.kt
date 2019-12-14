eab61380366f3f8a1fd758a3cc2015ff9416c809
imran <imrankhanissm@gmail.com>
2019-12-14 13:20:25 +0530

    count down added to alert dialog

--- app/src/main/java/com/example/bikeapp/services/MyIntentService.kt
+++ app/src/main/java/com/example/bikeapp/services/MyIntentService.kt
@@ -11,18 +11,20 @@ import android.os.Build
 import android.os.SystemClock
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.example.bikeapp.R
 
 class MyIntentService : IntentService("MyIntentService") {
 
     @TargetApi(Build.VERSION_CODES.O)
     override fun onHandleIntent(intent: Intent?) {
-        var notChannel = NotificationChannel("ch1", "channel1", NotificationManager.IMPORTANCE_DEFAULT)
+        var channelId = "ch1"
+        var notChannel = NotificationChannel(channelId, "channel1", NotificationManager.IMPORTANCE_DEFAULT)
         var notMng = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        var not = Notification.Builder(this).setContentTitle("myNot")
+        var not = NotificationCompat.Builder(this, channelId).setContentTitle("myNot")
             .setContentText("myNotText")
             .setSmallIcon(R.drawable.ic_my_location_blue_24dp)
-            .setChannelId("ch1")
+            .setChannelId(channelId)
             .build()
         notMng.createNotificationChannel(notChannel)
         notMng.notify(1, not)
@@ -35,6 +37,6 @@ class MyIntentService : IntentService("MyIntentService") {
         }
 
         Log.d("debug", "onHandleIntent")
-        Toast.makeText(this, "onHangleIntent", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "onHandleIntent", Toast.LENGTH_SHORT).show()
     }
 }
